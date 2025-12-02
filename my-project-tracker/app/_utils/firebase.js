46c9650a98a525666656d9052536f9c0fd2df153
Nick <nicholas.brunet@edu.sait.ca>
2025-12-02 16:47:10 -0700

    Basic Firebase Authentication Sign In

---
+++ my-project-tracker/app/_utils/firebase.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+import { getAuth } from "firebase/auth";
+ 
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyC3DKdTD_Fj3dspTgUQ-4XbVLM3OaM1wvI",
+  authDomain: "my-project-tracker-7639c.firebaseapp.com",
+  projectId: "my-project-tracker-7639c",
+  storageBucket: "my-project-tracker-7639c.firebasestorage.app",
+  messagingSenderId: "241382270610",
+  appId: "1:241382270610:web:23b4f9916c1d211b63a84b"
+};
+ 
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+export const auth = getAuth(app);
--- my-project-tracker/app/layout.tsx
+++ my-project-tracker/app/layout.tsx
@@ -1,5 +1,6 @@
 import type { Metadata } from "next";
 import { Geist, Geist_Mono } from "next/font/google";
+import { AuthContextProvider } from "./_utils/auth-context";
 import "./globals.css";
 
 const geistSans = Geist({
@@ -27,7 +28,9 @@ export default function RootLayout({
       <body
         className={`${geistSans.variable} ${geistMono.variable} antialiased`}
       >
-        {children}
+        <AuthContextProvider>
+          {children}
+        </AuthContextProvider>
       </body>
     </html>
   );
