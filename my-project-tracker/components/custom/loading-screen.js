fdf7ac07feab8f9868a3a6ac8b95d46eb3b1794d
FIraol-A <firaol.ahmed@edu.sait.ca>
2025-12-10 20:23:29 -0700

    Projects now link to target project page. Created project details page, fetches target project data. Added logo to loading screen. Minor changes to dashboard.

--- my-project-tracker/components/custom/loading-screen.js
+++ my-project-tracker/components/custom/loading-screen.js
@@ -1,17 +1,20 @@
-import {
-  Dialog,
-  DialogTitle,
-  DialogContent
-} from "@/components/ui/dialog"
-import { Spinner } from "@/components/ui/spinner"
+import { Dialog, DialogTitle, DialogContent } from "@/components/ui/dialog";
+import { Spinner } from "@/components/ui/spinner";
+import Image from "next/image";
 
-export default function LoadingScreen({ enabled }){
-  return(
+export default function LoadingScreen({ enabled }) {
+  return (
     <Dialog open={enabled}>
       <DialogContent className="flex w-1/8 justify-center">
-        <Spinner/>
+        {/*<Image*/}
+        {/*  src={"my-project-tracker/public/my-project-tracker-logo.png"}*/}
+        {/*  alt={"Hello"}*/}
+        {/*  width={100}*/}
+        {/*  height={100}*/}
+        {/*/>*/}
+        <Spinner />
         <DialogTitle>Loading...</DialogTitle>
       </DialogContent>
     </Dialog>
-  )
+  );
 }
--- my-project-tracker/components/custom/project-card.js
+++ my-project-tracker/components/custom/project-card.js
@@ -1,5 +1,57 @@
-function ProjectCard({project}) {
-    return (
-        <div></div>
-    )
+"use client";
+
+import {
+  Card,
+  CardContent,
+  CardDescription,
+  CardHeader,
+  CardTitle,
+} from "../ui/card";
+import { DataTable } from "../../app/project/data-table";
+import { columns } from "../../app/project/columns";
+import ProfileCard from "./profile-card";
+
+const formatDate = (dateNum) => {
+  if (!dateNum) return "N/A";
+  return new Date(dateNum).toLocaleDateString("en-CA", {
+    year: "numeric",
+    month: "long",
+    day: "numeric",
+  });
+};
+
+// Change to show availability tasks
+const tasks = [];
+export default function ProjectCard({ project }) {
+  return (
+    <div className={"flex flex-col gap-4"}>
+      <Card>
+        <CardHeader>
+          <div className="flex items-start justify-between">
+            <div>
+              <CardTitle className="text-3xl font-bold">
+                {project.name || "Untitled Project"}
+              </CardTitle>
+              <CardDescription className="mt-2 text-base">
+                {project.description || "No description provided."}
+              </CardDescription>
+            </div>
+          </div>
+        </CardHeader>
+        <CardContent className="">
+          <div className="grid grid-cols-2 gap-4 rounded-lg border bg-white p-4">
+            <div>
+              <p className="text-sm font-medium text-gray-500">Created</p>
+              <p>{formatDate(project.dateCreated)}</p>
+            </div>
+            <div>
+              <p className="text-sm font-medium text-gray-500">Due Date</p>
+              <p>{formatDate(project.dateDue)}</p>
+            </div>
+          </div>
+        </CardContent>
+      </Card>
+      <DataTable columns={columns} data={tasks} />
+    </div>
+  );
 }
